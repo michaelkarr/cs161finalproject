9c676679ac821663b6d34f7904f97d4a8ab2b2ad
Michael Karr <mkarr1@yahoo.com>
2018-03-14 14:35:58 -0700

    partner files and fast backbone no path

--- CLCSFast.py
+++ CLCSFast.py
@@ -1,32 +1,36 @@
 import sys
 import numpy as np
-import logging
 
 """
 Currently just runs LCS
 TODO: implement CLCSFast algorithm
 """
 
-def LCSBig(A,B,arr):
+# used for calculating each DP
+def disLCS(A, B, dis, arr):
 	m = len(A)
 	n = len(B)
 
 	A = A + A
 
-	for i in range(1,2 * m+1):
+	# 0 out row before for calculation
+	# do not need to 0 out first col bc always 0
+	arr[dis - 1] = 0
+
+	for i in range(1,m+1):
 		for j in range(1,n+1):
-			if A[i-1] == B[j-1]:
-				arr[i][j] = arr[i-1][j-1]+1
+			if A[dis + i-1] == B[j-1]:
+				arr[dis + i][j] = arr[dis + i-1][j-1]+1
 			else:
-				arr[i][j] = max(arr[i-1][j], arr[i][j-1])
+				arr[dis + i][j] = max(arr[dis + i-1][j], arr[dis + i][j-1])
 
-	# return max(arr[i + m][n] for i in range(m))
-	return arr[m][n]
+	return arr[m + dis][n]
 
-def backtracePath(A, B, i, j, arr):  # dim of array
+# potentially backtrace twice for upper and lower bounds
+def backtracePath(A, B, i, j, dis, arr):  # dis = displacement to calculate
 	# TODO: currently nonbounded, need to add 
 	path = np.zeroes((i,), dtype=int)
-	while i > 0 and j > 0:
+	while i > 0 + dis and j > 0:
 		if A[i-1] == B[j-1]:
 			path[i - 1] = j - 1
 			i -= 1
@@ -37,17 +41,22 @@ def backtracePath(A, B, i, j, arr):  # dim of array
 		else:
 			path[i] = j - 1
 			j -= 1
+	return path
 
 
-def singleShortestPath(A, B, m, pl, pu):
+def singleShortestPath(A, B, m, pl, pu, arr, pValDict):
 	# computes pm by running the DP on the table bounded by pl and pu
+	# step 1: compute dp
+	val = disLCS(A, B, m, arr)
+	pValDict[m] = val
+	# step 2: backtracePath(A, B, i, j, dis, arr) and return the path
 	return
 
-def findShortestPaths(A, B, p, l, u, arr):
+def findShortestPaths(A, B, p, l, u, arr, pValDict):
 	if u - l <= 1:
 		return
 	mid = int((l + u) / 2)
-	p[mid] = singleShortestPath(A, B, mid, p[l], p[u])
+	p[mid] = singleShortestPath(A, B, mid, p[l], p[u], arr, pValDict)
 	findShortestPaths(A, B, p, l, mid)
 	findShortestPaths(A, B, p, mid, u)
 
@@ -55,7 +64,6 @@ def cut(s, i):
     return s[i:] + s[0:i]
 
 def setArr(m, n):
-	# TODO: check dimensions
 	return np.zeros((2 * m + 1, (n + 1)), dtype=int)
 
 def setP(m, n):
@@ -70,13 +78,17 @@ def main():
 		if len(A) > len(B):
 			A, B = B, A
 		arr = setArr(len(A), len(B))
-		# cut A at all possible locations, B constant
-		# take the maximum path length of those computed
-		# TODO: should we be checking which is longer
-		maxLength = max([LCSBig(cut(A, j), B, arr) for j in range(len(A))])
-		# TODO: this code should run
-		# findShortestPaths(A, B, p, 0, len(A))
-		# return the shortest pi
+
+		# THIS PIECE OF CODE IS TEMPORARY, JUST SO IT COMPILES RIGHT NOW
+		maxLength = max([disLCS(cut(A, j), B, 0, arr) for j in range(len(A))])
+
+		# TODO: this code should run, not above line
+		p = setP(len(A), len(B))
+		pValDict = dict()
+		# p[0] is the backtrace of standard LCS
+		# p[-1] is the same as p[0] but rows shifted down m
+		# call findShortestPaths(A, B, p, 0, len(A), pValDict) to fill p array
+		# return max([value for key, value in pValDict.iteritems()])
 		print maxLength
 	return
 
